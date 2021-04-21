c000ec3d2ad328776e48b219f124be3e5057a16b
zahra006 <zahratul.ainiakbar@gmail.com>
2021-04-21 11:36:51 +0700

    initial commit

--- app/Http/Controllers/Api/CommentsController.php
+++ app/Http/Controllers/Api/CommentsController.php
@@ -71,7 +71,7 @@ class CommentsController extends Controller
 
         return response()->json([
             'success'=> true,
-            'comment'=> $comments
+            'comments'=> $comments
         ]);
     }
 }
